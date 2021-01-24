c998f1d6b504e0523ff18959e48e2bd7c8356ce7
Zdravko Šplajt <zdravek.sprite@gmail.com>
2021-01-24 18:10:05 +0100

    Add task to server [React JS Crash Course 2021]

--- TraversyMedia/README.md
+++ TraversyMedia/README.md
@@ -55,7 +55,7 @@ npm i json-server
 npm run server
 ```
 - [x] 1:25:53​ - useEffect Hook & Fetch tasks from server
-- [ ] 1:30:13​ - Delete task from server
-- [ ] 1:31:51​ - Add task to server
+- [x] 1:30:13​ - Delete task from server
+- [x] 1:31:51​ - Add task to server
 - [ ] 1:35:15​ - Toggle reminder on server
 - [ ] 1:39:15​ - Routing, footer & about
--- TraversyMedia/react-task-tracker/src/App.js
+++ TraversyMedia/react-task-tracker/src/App.js
@@ -25,12 +25,20 @@ function App() {
   }
 
   // Add Task
-  const addTask = (task) => {
-    //console.log('add', task)
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
   }
+
   // Delete Task
   const deleteTask = async (id) => {
     await fetch(`http://localhost:5000/tasks/${id}`, {
